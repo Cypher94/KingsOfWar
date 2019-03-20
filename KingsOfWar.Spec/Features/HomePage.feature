49738cd96ae0dccd49eb94a737ac5b6b5666c313
Lech, Piotr <piotr.lech@accenture.com>
2019-03-20 11:06:33 +0100

    Added HomePage feature file and generated steps

---
+++ KingsOfWar.Spec/Features/HomePage.feature
@@ -0,0 +1,24 @@
+﻿Feature: HomePage
+	In order see informations about the game
+	As a Visitor
+	I want to have Home Page with game description and recent news
+
+Background:
+	Given I am on Home Page
+
+Scenario: Home Page is displayed correctly
+	Then I should see following elements displayed
+	| element           |
+	| Navigation        |
+	| Game info section |
+	| Articles Section  |
+	| Footer            |
+
+Scenario: Articles are displayed correctly
+	Then I should see Articles with following elements displayed
+	| element         |
+	| Title           |
+	| Description     |
+	| ReadMore button |
+
+#TODO: Test readmore and check single article when styled
---
+++ KingsOfWar.Spec/Steps/HomePageSteps.cs
@@ -0,0 +1,32 @@
+﻿using TechTalk.SpecFlow;
+
+namespace KingsOfWar.Spec.Steps
+{
+    [Binding]
+    public class HomePageSteps
+    {
+        #region Background
+        [Given(@"I am on Home Page")]
+        public void GivenIAmOnHomePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        #endregion
+
+        #region Home Page is displayed correctly
+        [Then(@"I should see following elements displayed")]
+        public void ThenIShouldSeeFollowingElementsDisplayed(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        #endregion
+
+        #region Articles are displayed correctly
+        [Then(@"I should see Articles with following elements displayed")]
+        public void ThenIShouldSeeArticlesWithFollowingElementsDisplayed(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        #endregion
+    }
+}
